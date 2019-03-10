670225ec5b7acbce80494638f413dc94b51cebaa
ConnerTenn <connertenn@gmail.com>
2019-03-10 17:09:02 -0400

    Working code

--- PiCode/main.cpp
+++ PiCode/main.cpp
@@ -1,5 +1,4 @@
 
-#include <iostream>
 #include <math.h>
 
 #include <stdint.h>
@@ -14,7 +13,6 @@
 #include <signal.h>
 #include <stdarg.h>
 #include <getopt.h>
-
 
 #include <clk.h>
 #include <gpio.h>
@@ -35,19 +33,19 @@ typedef unsigned short u16;
 typedef unsigned int u32;
 typedef unsigned long long u64;
 
-inline double RedVal(double val)
+double RedVal(double val)
 {
 	return val = 2.0-abs(6.0*(0.5-abs(val-0.5))), val=MIN(val,1.0), MAX(val,0.0);
 }
-inline double GreenVal(double val)
+double GreenVal(double val)
 {
 	return val = 2.0-abs(6.0*val-2.0), val=MIN(val,1.0), MAX(val,0.0);
 }
-inline double BlueVal(double val)
+double BlueVal(double val)
 {
 	return val = 2.0-abs(6.0*val-4.0), val=MIN(val,1.0), MAX(val,0.0);
 }
-inline u32 RGBVal(double val)
+u32 RGBVal(double val)
 {		
 	return (u8)(255.0*RedVal(val))<<16 | (u8)(255.0*GreenVal(val))<<8 | (u8)(255.0*BlueVal(val));
 }
@@ -56,13 +54,36 @@ inline u32 RGBVal(double val)
 #define GPIO_PIN                18
 #define DMA                     10
 #define STRIP_TYPE              WS2811_STRIP_GRB		// WS2812/SK6812RGB integrated chip+leds
-#define LED_COUNT 30
+#define LED_COUNT 300
 
 
 bool Run = true;
 ws2811_led_t Strip[LED_COUNT];
 
 ws2811_t ledstring;
+/*ws2811_t ledstring =
+{
+    .freq = TARGET_FREQ,
+    .dmanum = DMA,
+    .channel =
+    {
+        [0] =
+        {
+            .gpionum = GPIO_PIN,
+            .count = LED_COUNT,
+            .invert = 0,
+            .brightness = 255,
+            .strip_type = STRIP_TYPE,
+        },
+        [1] =
+        {
+            .gpionum = 0,
+            .count = 0,
+            .invert = 0,
+            .brightness = 0,
+        },
+    },
+};*/
 
 static void ctrl_c_handler(int signum)
 {
@@ -72,10 +93,11 @@ static void ctrl_c_handler(int signum)
 
 void setup_handlers()
 {
-	struct sigaction sa; //= { .sa_handler = ctrl_c_handler };
+	struct sigaction sa;// = { .sa_handler = ctrl_c_handler };
+	memset(&sa, 0, sizeof(sa));
 	sa.sa_handler = ctrl_c_handler;
-	sigaction(SIGINT, &sa, NULL);
-	sigaction(SIGTERM, &sa, NULL);
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGTERM, &sa, NULL);
 }
 
 void Clear()
@@ -90,9 +112,8 @@ void Update()
 {
 	for (int i = 0; i < LED_COUNT; i++)
 	{
-		static double t = 0; t+= 0.001;
+		static double t = 0; t+= 0.0001;
 		Strip[i] = RGBVal(fmod(i/(LED_COUNT-1.0) + t,1.0));
-		Strip[i] = 0;
 	}
 }
 
@@ -109,23 +130,33 @@ int main()
 	ledstring.freq = TARGET_FREQ;
 	ledstring.dmanum = DMA;
 	
-	ws2811_channel_t channel;
-	channel.gpionum = GPIO_PIN;
-	channel.count = LED_COUNT;
-	channel.invert = 0;
-	channel.brightness = 55;
-	channel.strip_type = STRIP_TYPE;
-	ledstring.channel[0] = channel;
+	ws2811_channel_t channel1, channel2;
+	memset(&channel1, 0, sizeof(channel1));
+	memset(&channel2, 0, sizeof(channel2));
+	channel1.gpionum = GPIO_PIN;
+	channel1.count = LED_COUNT;
+	channel1.invert = 0;
+	channel1.brightness = 255;
+	channel1.strip_type = STRIP_TYPE;
+
+	ledstring.channel[0] = channel1;
+	ledstring.channel[1] = channel2;
 	
 	ws2811_return_t ret;
 	
+	printf("Setup Handlers\n");
+
 	setup_handlers();
+
+	printf("Init\n");
 	
 	if((ret = ws2811_init(&ledstring)) != WS2811_SUCCESS)
 	{
-		std::cerr << "ws2811_init failed: " << ws2811_get_return_t_str(ret) << "\n";
+		fprintf(stderr, "ws2811_init failed: %s\n", ws2811_get_return_t_str(ret));
 		return ret;
 	}
+
+	printf("Main Loop\n");
 	
 	while (Run)
 	{
@@ -134,7 +165,7 @@ int main()
 
 		if ((ret = ws2811_render(&ledstring)) != WS2811_SUCCESS)
 		{
-			std::cerr << "ws2811_render failed: " << ws2811_get_return_t_str(ret) << "\n";
+			fprintf(stderr, "ws2811_render failed: %s\n", ws2811_get_return_t_str(ret));
 			break;
 		}
 		
@@ -142,18 +173,18 @@ int main()
 		usleep(1000000 / 15);
 	}
 	
-	std::cout << "Exiting...\n";
+	printf("Exiting...\n");
 
 
 	Clear();
 	Render();
 	ws2811_render(&ledstring);
 	
-	std::cout << "Cleared...\n";
+	printf("Cleared...\n");
 
 	ws2811_fini(&ledstring);
 	
-	std::cout << "Done\n";
+	printf("Done\n");
 
 	return 0;
 }
